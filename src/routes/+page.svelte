41b4d540cda4eff8844d2a19670e9f18078272cc
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-25 21:33:13 +0100

    Rename packing -> print, document -> layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Document from '$lib/components/Document.svelte';
-	import Packing from '$lib/components/Packing.svelte';
+	import Layout from '$lib/components/Layout.svelte';
+	import Print from '$lib/components/Print.svelte';
 
 	let images: { url: string; width: number; height: number }[] = $state([]);
 
@@ -12,37 +12,37 @@
 	const printer_width_cm = 15;
 	const printer_height_cm = 10;
 
-	let view = $state<'document' | 'packing'>('document');
+	let view = $state<'layout' | 'print'>('layout');
 </script>
 
 <div class="flex flex-col items-center justify-center bg-gray-600 p-10">
 	<div class="mb-6 flex gap-4">
 		<button
 			class="rounded bg-gray-500 px-4 py-2 text-white opacity-50 disabled:opacity-100"
-			onclick={() => (view = 'document')}
-			disabled={view === 'document'}
+			onclick={() => (view = 'layout')}
+			disabled={view === 'layout'}
 		>
 			Layout
 		</button>
 		<button
 			class="rounded bg-gray-500 px-4 py-2 text-white opacity-50 disabled:opacity-100"
-			onclick={() => (view = 'packing')}
-			disabled={view === 'packing'}
+			onclick={() => (view = 'print')}
+			disabled={view === 'print'}
 		>
 			Print
 		</button>
 	</div>
 
 	<!-- Hide components instead of removing to save state -->
-	<div class={`${view === 'document' ? 'block' : 'hidden'} w-full`}>
-		<Document
+	<div class={`${view === 'layout' ? 'block' : 'hidden'} w-full`}>
+		<Layout
 			width={album_width_cm * resolution_pxcm}
 			height={album_height_cm * resolution_pxcm}
 			bind:images
 		/>
 	</div>
-	<div class={`${view === 'packing' ? 'block' : 'hidden'} w-full`}>
-		<Packing
+	<div class={`${view === 'print' ? 'block' : 'hidden'} w-full`}>
+		<Print
 			width={printer_width_cm * resolution_pxcm}
 			height={printer_height_cm * resolution_pxcm}
 			{images}
