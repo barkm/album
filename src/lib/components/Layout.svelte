d1d0962aa91245ced8a2ef99a5154521de206094
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-25 22:42:14 +0100

    Add multiple pages

--- src/lib/components/Layout.svelte
+++ src/lib/components/Layout.svelte
@@ -4,10 +4,28 @@
 	interface Props {
 		width: number;
 		height: number;
-		images?: { url: string; width: number; height: number }[];
+		images: { url: string; width: number; height: number }[];
 	}
 
 	let { width, height, images = $bindable([]) }: Props = $props();
+
+	let pages_images = $state([[]]);
+
+	$effect(() => {
+		images = pages_images.flat();
+	});
 </script>
 
-<Page {width} {height} bind:images />
+<div class="flex min-h-screen w-full flex-col justify-center gap-4">
+	{#each pages_images as _, page_index}
+		<Page {width} {height} bind:images={pages_images[page_index]} />
+	{/each}
+	<div class="flex justify-center">
+		<button
+			class="rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-400"
+			onclick={() => pages_images.push([])}
+		>
+			Add page
+		</button>
+	</div>
+</div>
