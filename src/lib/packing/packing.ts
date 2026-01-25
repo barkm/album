9aa68e8eca4ce715dde121569f27bf37d55363aa
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-25 19:32:41 +0100

    Move padding logic out from helper

--- src/lib/packing/packing.ts
+++ src/lib/packing/packing.ts
@@ -16,15 +16,19 @@ export const pack = <R extends Rectangle>(
 ): PackedRectangle<R>[][] => {
 	const border_padding = options?.border_padding ?? 0;
 	const rectangle_padding = options?.rectangle_padding ?? 0;
-	const bins = packHelper(
-		rectangles,
-		width - border_padding * 2,
-		height - border_padding * 2,
-		rectangle_padding
-	);
+	rectangles = rectangles.map((rect) => ({
+		...rect,
+		width: rect.width + rectangle_padding,
+		height: rect.height + rectangle_padding
+	}));
+	width = width - border_padding * 2 + rectangle_padding;
+	height = height - border_padding * 2 + rectangle_padding;
+	const bins = packHelper(rectangles, width, height);
 	return bins.map((bin) =>
 		bin.map((rect) => ({
 			...rect,
+			width: rect.width - rectangle_padding,
+			height: rect.height - rectangle_padding,
 			x: rect.x + border_padding,
 			y: rect.y + border_padding
 		}))
@@ -34,8 +38,7 @@ export const pack = <R extends Rectangle>(
 const packHelper = <R extends Rectangle>(
 	rectangles: R[],
 	width: number,
-	height: number,
-	rectangle_padding: number
+	height: number
 ): PackedRectangle<R>[][] => {
 	if (rectangles.length === 0) {
 		return [];
@@ -48,27 +51,27 @@ const packHelper = <R extends Rectangle>(
 	}
 	let packed_rectangles: PackedRectangle<R>[][] = [];
 	let current_bin: PackedRectangle<R>[] = [{ ...rectangles[0], x: 0, y: 0 }];
-	let current_x = rectangles[0].width + rectangle_padding;
+	let current_x = rectangles[0].width;
 	let current_shelf_floor = 0;
-	let current_shelf_height = rectangles[0].height + rectangle_padding;
+	let current_shelf_height = rectangles[0].height;
 	for (let i = 1; i < rectangles.length; i++) {
 		const rect = rectangles[i];
 		if (current_x + rect.width <= width && current_shelf_floor + rect.height <= height) {
 			current_bin.push({ ...rect, x: current_x, y: current_shelf_floor });
-			current_x += rect.width + rectangle_padding;
+			current_x += rect.width;
 			current_shelf_height = Math.max(current_shelf_height, rect.height);
 		} else if (current_shelf_floor + current_shelf_height + rect.height <= height) {
 			current_x = 0;
 			current_shelf_floor += current_shelf_height;
-			current_shelf_height = rect.height + rectangle_padding;
+			current_shelf_height = rect.height;
 			current_bin.push({ ...rect, x: current_x, y: current_shelf_floor });
-			current_x += rect.width + rectangle_padding;
+			current_x += rect.width;
 		} else {
 			packed_rectangles.push(current_bin);
 			current_bin = [{ ...rect, x: 0, y: 0 }];
-			current_x = rect.width + rectangle_padding;
+			current_x = rect.width;
 			current_shelf_floor = 0;
-			current_shelf_height = rect.height + rectangle_padding;
+			current_shelf_height = rect.height;
 		}
 	}
 	packed_rectangles.push(current_bin);
