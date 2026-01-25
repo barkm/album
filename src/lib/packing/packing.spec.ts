f6a3cd177e7a5961167716a0eab059f9d57b05a0
Patrik Barkman <patrik.barkman@volumental.com>
2026-01-25 17:25:01 +0100

    Add multiple rectangles fit test

--- src/lib/packing/packing.spec.ts
+++ src/lib/packing/packing.spec.ts
@@ -15,4 +15,13 @@ describe('packing test', () => {
 		const rectangles = [{ width: 15, height: 5 }];
 		expect(() => pack(rectangles, 10, 10)).toThrow();
 	});
+
+	it('multiple rectangles fit', () => {
+		const rectangles = [
+			{ width: 5, height: 5 },
+			{ width: 3, height: 3 },
+			{ width: 2, height: 2 }
+		];
+		expect(pack(rectangles, 10, 10)).toEqual([rectangles]);
+	});
 });
